e38bb78c0727c3fab03da453c02ae70ae918a748
Kristin Yap <144386730+kmyap128@users.noreply.github.com>
2025-11-04 12:11:22 -0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/material.dart';
+import 'page_profile.dart';
+import 'page_search.dart';
+import 'page_settings.dart';
+import 'page_home.dart';
+
+int currentBottomTab = 0;
+
+final List<Widget> bottomNavScreens = [
+  PageHome(),
+  PageSearch(),
+  PageSettings(),
+  PageProfile()
+];
 
 void main() {
   runApp(const MainApp());
@@ -29,8 +42,21 @@ class _Demo11aState extends State<Demo11a> {
         appBar:AppBar(
           title: Text("Week 11B Demo"),
         ),
-        body: Center(
-          child: Text('Hello World!'),
+        body: bottomNavScreens[currentBottomTab],
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.shifting,
+          currentIndex: currentBottomTab,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home", backgroundColor: Colors.green),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search", backgroundColor: Colors.yellow),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings", backgroundColor: Colors.orange),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile", backgroundColor: Colors.brown)
+          ],
+          onTap: (value) {
+            setState(() {
+              currentBottomTab = value;
+            });
+          },
         ),
     );
   }
